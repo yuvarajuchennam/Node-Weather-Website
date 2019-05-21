2513eed319ab5c5538885e224928b3284d7a39ff
yuvaraju chennam <chennamyuvaraju1@gmail.com>
2019-05-21 12:05:46 +0530

    change settings to heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -97,6 +98,6 @@ app.get('*', (req, res) => {
         errorMessage:'404  Page not found'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 })
